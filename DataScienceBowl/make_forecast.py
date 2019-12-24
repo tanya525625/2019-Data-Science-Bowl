6e9763f16d1393e32b70b7793fbb7e83da6ea99b
Zubakhina Tatyana <33422677+tanya525625@users.noreply.github.com>
2019-12-24 19:30:37 +0500

    Created FileWorker

--- DataScienceBowl/make_forecast.py
+++ DataScienceBowl/make_forecast.py
@@ -1,3 +1,4 @@
+import os
 from DataScienceBowl.tools.file_worker import read_data
 
 
@@ -6,8 +7,8 @@ def make_forecast(data):
 
 
 if __name__ == "__main__":
-    input_path = "../Data"
-    output_path = "../Prediction"
+    input_path = os.path.join("..", "Data")
+    output_path = os.path.join("..", "Prediction")
     files = ("sample_submission.csv", "test.csv", "train.csv", "train_labels.csv")
     data = read_data(files, input_path)
     make_forecast(data)
