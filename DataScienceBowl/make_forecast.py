fe29c876491eb25dfb4660638531aa137188ed66
Baynova Ekaterina <ekaterinabainova@gmail.com>
2020-01-01 21:32:39 +0300

    Added counting of columns with wissing values

--- DataScienceBowl/make_forecast.py
+++ DataScienceBowl/make_forecast.py
@@ -1,6 +1,7 @@
 import os
 
 from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
 
 from tools.quadratic_metric_kappa import quadratic_kappa
 from tools.quadratic_metric_kappa import list_of_class_values_from_file
@@ -8,6 +9,7 @@ from tools.file_worker import read_data, FileWorker
 from tools.null_processing import drop_nones
 from tools.file_worker import write_submission
 from tools.make_model import ModelMaker
+from tools.prepare_data import missing_values_table
 
 
 def make_forecast(data: dict):
@@ -43,8 +45,15 @@ if __name__ == "__main__":
     # files = ("sample_submission.csv", "test.csv",
     #          "train.csv", "train_labels.csv")
     # data = read_data(files, input_path)
-    data = {}
-    make_forecast(data)
+
+    # data = {}
+    # make_forecast(data)
+
+    df = pd.read_csv("../Data/train.csv")
+    mis_columns = missing_values_table(df)
+
+    print(mis_columns)
+
     #
     # actuals = list_of_class_values_from_file(actuals_path)
     # preds = list_of_class_values_from_file(preds_path)
