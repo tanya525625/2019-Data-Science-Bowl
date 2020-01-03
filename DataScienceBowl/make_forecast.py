673788fbaaca8d17fb7b164dfe138f3ca55950f1
Rexarrior <Rexarrior@yandex.ru>
2020-01-04 00:21:09 +0300

    1. Method for exploration hyperparams and model types. 2. Cached verion of drop_nones method

--- DataScienceBowl/make_forecast.py
+++ DataScienceBowl/make_forecast.py
@@ -5,17 +5,14 @@ from sklearn.neighbors import KNeighborsClassifier
 from tools.quadratic_metric_kappa import quadratic_kappa
 from tools.quadratic_metric_kappa import list_of_class_values_from_file
 from tools.file_worker import read_data, FileWorker
-from tools.null_processing import drop_nones
+from tools.null_processing import cacheable_drop_nones
 from tools.file_worker import write_submission
 from tools.make_model import ModelMaker
 
 
-def make_forecast(data: dict):
-    # train_dataset = drop_nones(data)
-
-    # write dataset if it's necessary
-    # fw.write_df(train_dataset, "new_train.csv")
-
+def make_forecast(data_path: str, data_files: list):
+    fw = FileWorker
+    train_dataset = cacheable_drop_nones(data_path, data_files)
     hyperparams = {
         "n_neighbors": 5,
         "weights": "uniform",
@@ -24,8 +21,6 @@ def make_forecast(data: dict):
         "p": 2,
         "metric": "minkowski",
     }
-    fw = FileWorker
-    train_dataset = fw.read_df("new_train.csv")
     model = ModelMaker(KNeighborsClassifier, hyperparams, train_dataset)
     prediction = model.predict()
     write_submission(model.test_ist_ids, prediction, "submission.csv")
@@ -34,17 +29,18 @@ def make_forecast(data: dict):
 
 
 if __name__ == "__main__":
-    input_path = os.path.join("..", "Data")
-    output_path = os.path.join("..", "Prediction")
+    input_path = os.path.join("./", "Data")
+    output_path = os.path.join(".", "Prediction")
 
     actuals_path = os.path.join(input_path, "sample_submission.csv")
     preds_path = os.path.join(output_path, "predictions.csv")
 
-    # files = ("sample_submission.csv", "test.csv",
-    #          "train.csv", "train_labels.csv")
+    files = ["sample_submission.csv", "test.csv",
+             "train.csv", "train_labels.csv"]
     # data = read_data(files, input_path)
-    data = {}
-    make_forecast(data)
+    # data['train.csv'] = data['train_part.csv']
+    # data['train_part.csv'] = None
+    make_forecast(input_path, files)
     #
     # actuals = list_of_class_values_from_file(actuals_path)
     # preds = list_of_class_values_from_file(preds_path)
